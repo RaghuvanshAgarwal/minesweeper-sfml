ec638fd1cbebf4e5c3229d6d62aecf763e539ee0
Raghuvansh Agarwal <rarchva@gmail.com>
2025-10-10 22:10:59 +0530

    Drawing Grid

--- source/Gameplay/Board.cpp
+++ source/Gameplay/Board.cpp
@@ -9,16 +9,16 @@
 #include "../../header/Assets/AssetManager.h"
 
 namespace Gameplay {
-    void Board::initializeBoardImage(const sf::RenderWindow* window) {
+    void Board::initializeBoardImage(const sf::RenderWindow *window) {
         if (!texture_.loadFromFile(Asset::AssetManager::getTexture(Asset::TextureType::Board))) {
             throw std::runtime_error("Failed to load board image!");
         }
         sprite_.setTexture(texture_);
         sprite_.setPosition(board_position_, 0);
-        sprite_.setScale(board_width_ / texture_.getSize().x,board_height_ / texture_.getSize().y);
+        sprite_.setScale(board_width_ / texture_.getSize().x, board_height_ / texture_.getSize().y);
     }
 
-    void Board::initialize(sf::RenderWindow* window) {
+    void Board::initialize(sf::RenderWindow *window) {
         initializeBoardImage(window);
         createBoard();
     }
@@ -36,19 +36,23 @@ namespace Gameplay {
         const float cell_height = getCellHeight();
 
         for (int i = 0; i < number_of_rows; i++) {
-            cell_[i] = new Cell(cell_width,cell_height,sf::Vector2i(i,0));
+            for (int j = 0; j < number_of_columns; j++) {
+                cell_[i][j] = new Cell(cell_width, cell_height, sf::Vector2i(i, j));
+            }
         }
     }
 
-    Board::Board(sf::RenderWindow* window) {
+    Board::Board(sf::RenderWindow *window) {
         initialize(window);
     }
 
 
     void Board::render(sf::RenderWindow &window) const {
         window.draw(sprite_);
-        for (int i = 0; i < number_of_rows; i++) {
-            cell_[i]->render(window);
+        for (const auto & i : cell_) {
+            for (const auto j : i) {
+                j->render(window);
+            }
         }
     }
 } // Gameplay
